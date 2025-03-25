70fbfa0ed4668b00925a77dd4700b9da7b99985b
花楹一间 <yanl_802@163.com>
2025-03-25 14:00:46 +0800

    refactor(updater): 优化更新器配置和功能
    - 添加新版本下载询问通知
    - 处理无可用更新情况
    - 显示下载进度
    - 增加错误处理
    - 移除冗余代码和注释

--- source/electron/tools/updater.ts
+++ source/electron/tools/updater.ts
@@ -6,17 +6,23 @@ import electronUpdater, {
 } from 'electron-updater';
 import { getIsPackage } from '../helpers/app';
 
+// async function sleep(ms: number) {
+//   return new Promise((resolve) => {
+//     const timer = setTimeout(() => {
+//       resolve(true);
+//       clearTimeout(timer);
+//     }, ms);
+//   });
+// }
+
 export function getAutoUpdater(): AppUpdater {
   const { autoUpdater } = electronUpdater;
   Logger.transports.file.level = 'debug';
   Logger.transports.file.format =
     '[{y}-{m}-{d} {h}:{i}:{s}.{ms}] [{level}] {scope} {text}';
   Logger.transports.file.resolvePathFn = () => './update.log';
-  // const notifer = new Notification({
-  //   title: '测试',
-  //   body: 'update',
-  // });
-  // notifer.show();
+  autoUpdater.disableWebInstaller = false;
+  autoUpdater.autoDownload = false;
   autoUpdater.logger = Logger;
   return autoUpdater;
 }
@@ -26,6 +32,8 @@ export function update(window?: BrowserWindow | undefined) {
     return;
   }
   const autoUpdaterInstance = getAutoUpdater();
+
+  // Tip: 一般情况下不需要 setFeedURL，也不建议使用，Electron-builder 会自动识别与处理
   // autoUpdaterInstance.setFeedURL({
   //   provider: 'github',
   //   owner: 'Aurora-flower',
@@ -33,25 +41,44 @@ export function update(window?: BrowserWindow | undefined) {
   // });
   autoUpdaterInstance.checkForUpdatesAndNotify({
     title: '更新提示',
-    body: '检测到新版本，是否更新？',
+    body: '检测到新版本',
   });
-  // autoUpdaterInstance.on('update-available', () => {
-  //   autoUpdaterInstance.downloadUpdate()
-  // })
+
+  autoUpdaterInstance.on('update-available', () => {
+    // Tip: 有时下载会过快，开启自动下载时会直接出现更新完成的提示。
+    const notification = new Notification({
+      title: '下载询问',
+      body: '检测到新版本，是否下载？',
+    });
+    notification.show();
+    notification.on('click', () => {
+      Logger.info('新版本下载中...');
+      autoUpdaterInstance.downloadUpdate();
+    });
+  });
+
+  autoUpdaterInstance.on('update-not-available', () => {
+    Logger.info('没有可用更新');
+  });
+
   autoUpdaterInstance.on('download-progress', (progress) => {
-    Logger.info('download-progress');
+    Logger.info('download-progress', progress.percent);
     window.setProgressBar(progress.percent / 100);
   });
+
   autoUpdaterInstance.on('update-downloaded', () => {
     Logger.info('update-downloaded');
     const notification = new Notification({
       title: '更新提示',
       body: '更新已下载完成，是否重启应用进行安装？',
     });
+    notification.show();
     notification.on('click', () => {
       autoUpdaterInstance.quitAndInstall();
     });
   });
 
-  Logger.info('update');
+  autoUpdaterInstance.on('error', (error) => {
+    Logger.error('error', error);
+  });
 }
