78348ad3244b90b060f0f4a8563a187e197c5c30
花楹一间 <yanl_802@163.com>
2025-03-25 11:25:57 +0800

    feat(update): 添加软件更新提示和下载进度显示
    - 修改 appId 为 com.huaying.electron-vue-vite
    - 在更新时添加确认提示和下载进度显示
    - 下载完成后弹出通知，点击可重启应用进行安装

--- source/electron/tools/updater.ts
+++ source/electron/tools/updater.ts
@@ -1,8 +1,5 @@
 import Logger from 'electron-log';
-import {
-  BrowserWindow,
-  // Notification
-} from 'electron';
+import { BrowserWindow, Notification } from 'electron';
 import electronUpdater, {
   type AppUpdater,
   // NsisUpdater,
@@ -34,7 +31,27 @@ export function update(window?: BrowserWindow | undefined) {
   //   owner: 'Aurora-flower',
   //   repo: 'Electron-Vue3-Vite',
   // });
-  autoUpdaterInstance.checkForUpdatesAndNotify();
+  autoUpdaterInstance.checkForUpdatesAndNotify({
+    title: '更新提示',
+    body: '检测到新版本，是否更新？',
+  });
+  // autoUpdaterInstance.on('update-available', () => {
+  //   autoUpdaterInstance.downloadUpdate()
+  // })
+  autoUpdaterInstance.on('download-progress', (progress) => {
+    Logger.info('download-progress');
+    window.setProgressBar(progress.percent / 100);
+  });
+  autoUpdaterInstance.on('update-downloaded', () => {
+    Logger.info('update-downloaded');
+    const notification = new Notification({
+      title: '更新提示',
+      body: '更新已下载完成，是否重启应用进行安装？',
+    });
+    notification.on('click', () => {
+      autoUpdaterInstance.quitAndInstall();
+    });
+  });
 
   Logger.info('update');
 }
